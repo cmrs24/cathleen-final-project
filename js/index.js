a499e426f6ffcae66019be414f483f484c9f7950
cmrs24 <cathleen2479@gmail.com>
2017-05-25 20:35:22 -0400

    commit

--- js/index.js
+++ js/index.js
@@ -30,37 +30,50 @@ $(function(){
 
 
 //set price variables
-   var regionPrice;
-   var typePrice;
-   var resurfacePrice;
-   var surfacePrice;   
+   var regionPrice = 0;
+   var typePrice = 0; 
+   var surfacePrice = 0; 
+   var resurfacePrice = 0;
 
 //pool type stuff
 
 var poolType = ["above-ground", "in-ground", "other"];
 
-  $('form').on('change', '#pool-type',function(){
+$('form').on('change', '#pool-type',function(){
 
-    var poolType = $("input[name='pool-type']:checked").val();
+  var poolType = $("input[name='pool-type']:checked").val();
 
-    if(poolType == 'above-ground') {
-      typePrice="100";
-      console.log(typePrice);
-    }
+  if(poolType == 'above-ground') {
+    typePrice = 100;
+    console.log(typePrice);
+  }
 
-    else if
-      (poolType == 'in-ground') {
-      typePrice="200";
-      console.log(typePrice);
-    }
+  else if
+    (poolType == 'in-ground') {
+    typePrice = 200;
+    console.log(typePrice);
+  }
 
-    else if 
-      (poolType == 'other') {
-      typePrice="0";
-      console.log(typePrice);
-    }
+  else if 
+    (poolType == 'other') {
+    typePrice = 0;
+    console.log(typePrice);
+  }
 
-  });
+});
+
+
+//slider type stuff
+
+// var resurfaceYear = [1,2,3,4,5,6,7,8,9,10];
+
+// $('form').on('change', '#defaultval', function(){
+
+//   var resurfaceYear = $("#defaultval").slider("value");
+//   console.log(value)
+//   resurfacePrice= sliderYear * 20;
+
+// });
 
 //surface type stuff
 
@@ -71,18 +84,22 @@ var surfaceType = ["pebble", "plaster", "other", "quartz"];
     var surfaceType = $("input[name='surface-type']:checked").val();
 
     if(surfaceType == 'pebble') {
-      surfacePrice="500";
+      surfacePrice=500;
       console.log(surfacePrice);
     }
     else if 
       (surfaceType == 'plaster') {
-      surfacePrice="100";
+      surfacePrice=100;
       console.log(surfacePrice);
-
+    }
+      else if 
+      (surfaceType == 'quartz') {
+      surfacePrice=100;
+      console.log(surfacePrice);
     }
     else if 
       (surfaceType == 'other') {
-      surfacePrice="0";
+      surfacePrice=0;
       console.log(surfacePrice);
     }
 
@@ -99,35 +116,45 @@ for(i=0;i<regionType.length;i++) {
   $('form').on('change', '#region-type', function(){
     var regionType = $('#region-type').val();
     if(regionType == 'Northeast') {
-    regionPrice="500";
-    console.log(regionPrice);
+      regionPrice = 500;
+      console.log(regionPrice);
     }
     else if (regionType == 'Southeast') {
-    regionPrice="600";
-    console.log(regionPrice);
+      regionPrice = 600;
+      console.log(regionPrice);
     }
     else if (regionType == 'West') {
-    regionPrice="900";
+    regionPrice = 900;
     console.log(regionPrice);
     }
     else if (regionType == 'Midwest') {
-    regionPrice="1000";
+    regionPrice = 1000;
     console.log(regionPrice);
 
     }
     else if (regionType == 'Southwest') {
-    regionPrice="2000";
+    regionPrice = 2000;
     console.log(regionPrice);
     }
 
   });
 
-//get total
+  //get total
 
-$("#submit-btn").click()
 
-var totalPrice = typePrice + surfacePrice + regionPrice
-console.log(totalPrice)
+
+
+//show on button click
+
+
+  $( "#submit-btn" ).click(function(e){
+      e.preventDefault();
+        var totalPrice = typePrice + surfacePrice + regionPrice + resurfacePrice;
+        $("#myModal").modal('show');
+        
+      });
+
+
 
 
 
